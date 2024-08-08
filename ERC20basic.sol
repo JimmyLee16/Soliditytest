344310c7141657b53d50da4df9928ea4f9f2591a
JimmyLee16 <120309277+JimmyLee16@users.noreply.github.com>
2024-08-08 10:57:17 +0700

    Update ERC20basic.sol

--- ERC20basic.sol
+++ ERC20basic.sol
@@ -1,26 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+contract SimpleToken {
+    string public constant name = "Vcoin";
+    string public constant symbol = "VCC";
+    uint8 public constant decimals = 18;
+    uint256 private _totalSupply;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-contract MyToken is IERC20 {
-    string public constant name = "Vccoin";
-    string public constant symbol = "VCC";
-    uint8 public constant decimals = 18;
-    
-    mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint256)) private _allowances;
-    uint256 private _totalSupply;
 
     constructor(uint256 initialSupply) {
         _totalSupply = initialSupply * 10 ** uint256(decimals);
@@ -28,39 +19,41 @@ contract MyToken is IERC20 {
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
 
-    function totalSupply() public view override returns (uint256) {
+    function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
 
-    function balanceOf(address account) public view override returns (uint256) {
+    function balanceOf(address account) public view returns (uint256) {
         return _balances[account];
     }
 
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-        require(_balances[msg.sender] >= amount, "ERC20: transfer amount exceeds balance");
-        
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        require(recipient != address(0), "Transfer to the zero address");
+        require(_balances[msg.sender] >= amount, "Transfer amount exceeds balance");
+
         _balances[msg.sender] -= amount;
         _balances[recipient] += amount;
         emit Transfer(msg.sender, recipient, amount);
         return true;
     }
 
-    function allowance(address owner, address spender) public view override returns (uint256) {
-        return _allowances[owner][spender];
-    }
+    function approve(address spender, uint256 amount) public returns (bool) {
+        require(spender != address(0), "Approve to the zero address");
 
-    function approve(address spender, uint256 amount) public override returns (bool) {
         _allowances[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
-    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-        require(_balances[sender] >= amount, "ERC20: transfer amount exceeds balance");
-        require(_allowances[sender][msg.sender] >= amount, "ERC20: transfer amount exceeds allowance");
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        require(sender != address(0), "Transfer from the zero address");
+        require(recipient != address(0), "Transfer to the zero address");
+        require(_balances[sender] >= amount, "Transfer amount exceeds balance");
+        require(_allowances[sender][msg.sender] >= amount, "Transfer amount exceeds allowance");
 
         _balances[sender] -= amount;
         _balances[recipient] += amount;
